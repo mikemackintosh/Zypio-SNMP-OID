a3abbe92c83d64ad0aefcd90c213a5c4c89caecd
Mike Mackintosh <mike.mackintosh@angrystatic.com>
2013-07-03 22:16:48 -0400

    Re-ogranized example.php
    Signed-off-by: Mike Mackintosh <mike.mackintosh@angrystatic.com>

--- example.php
+++ example.php
@@ -39,12 +39,12 @@ $snmp = new ZypioSNMP(".1.3.6.1.4.1.38741");
 $snmp->addOid(".1.0", ZypioSNMP::STRING, "ZypioPHP"); // .1.3.6.1.4.1.38741.1.0
 $snmp->addOid(".1.1", ZypioSNMP::INTEGER, 1); // .1.3.6.1.4.1.38741.1.1
 $snmp->addOid(".1.1.4.0.9.8.8.0", ZypioSNMP::STRING, "This is nice and long"); // .1.3.6.1.4.1.38741.1.1.4.0.9.8.8.0
-$snmp->addOid(".1.1.4.0.9.8.8.1", ZypioSNMP::STRING, "This is nice and long, with a continuance"); // .1.3.6.1.4.1.38741.1.1.4.0.9.8.8.0
-$snmp->addOid(".7.1.3.0", ZypioSNMP::IPADDR, "10.211.53.3"); // .1.3.6.1.4.1.38741.22.1.8.0
-$snmp->addOid(".8.1.8.0", ZypioSNMP::STRING, "Here is another string"); // .1.3.6.1.4.1.38741.8.1.8.0
-$snmp->addOid(".9.0", ZypioSNMP::GUAGE, rand(0,100)); // .1.3.6.1.4.1.38741.22.1.8.0
-$snmp->addOid(".32.0", ZypioSNMP::STRING, "unix timestamp"); // .1.3.6.1.4.1.38741.22.1.8.0
-$snmp->addOid(".32.1", ZypioSNMP::COUNTER, time()); // .1.3.6.1.4.1.38741.22.1.8.0
+$snmp->addOid(".1.1.4.0.9.8.8.1", ZypioSNMP::STRING, "This is nice and long, with a continuance"); // .1.3.6.1.4.1.38741.1.1.4.0.9.8.8.1
+$snmp->addOid(".6.1.3.0", ZypioSNMP::IPADDR, "10.211.53.3"); // .1.3.6.1.4.1.38741.6.1.3.0
+$snmp->addOid(".7.1.8.0", ZypioSNMP::STRING, "Here is another string"); // .1.3.6.1.4.1.38741.7.1.8.0
+$snmp->addOid(".8.2", ZypioSNMP::GUAGE, rand(0,100)); // .1.3.6.1.4.1.38741.8.2
+$snmp->addOid(".9.2.0", ZypioSNMP::STRING, "unix timestamp"); // .1.3.6.1.4.1.38741.22.1.9.2.0
+$snmp->addOid(".9.2.1", ZypioSNMP::COUNTER, time()); // .1.3.6.1.4.1.38741.22.1.9.2.1
 //*/
 
 // Respond
